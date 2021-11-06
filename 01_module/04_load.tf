c33a72f75ba6f462579da80f21687fc92b279422
majeong <alsdk5957@gmail.com>
2021-11-06 15:49:10 +0900

    step4.loadbalancer

--- 01_module/04_load.tf
+++ 01_module/04_load.tf
@@ -2,23 +2,20 @@
 resource "aws_lb" "minajeong_lb" {
   name               = "${var.name}-alb"
   internal           = false
-  load_balancer_type = "${var.lb_type}"
+  load_balancer_type = var.lb_type
   security_groups    = [aws_security_group.minajeong_websg.id]
-  count              = "${length(var.avazone)}"
   subnets            = [aws_subnet.minajeong_pub[0].id, aws_subnet.minajeong_pub[1].id]
-
 
   tags = {
     "Name" = "${var.name}-alb"
   }
 }
 
-
 # Load balancer target group
 resource "aws_lb_target_group" "minajeong_lbtg" {
   name     = "${var.name}-1btg"
-  port     = var.http_port
-  protocol = "HTTP"
+  port     = var.port_http
+  protocol = var.protocol_http1
   vpc_id   = aws_vpc.minajeong_vpc.id
 
   health_check {
@@ -28,19 +25,17 @@ resource "aws_lb_target_group" "minajeong_lbtg" {
     matcher             = "200"
     path                = "/health.html"
     port                = "traffic-port"
-    protocol            = "HTTP"
+    protocol            = var.protocol_http1
     timeout             = 2
     unhealthy_threshold = 2
   }
 }
 
-
 # Load balancer listener
 resource "aws_lb_listener" "minajeong_lblist" {
-  count = 2
-  load_balancer_arn = aws_lb.minajeong_lb[count.index].arn
-  port              = var.http_port
-  protocol          = "HTTP"
+  load_balancer_arn = aws_lb.minajeong_lb.arn
+  port              = var.port_http
+  protocol          = var.protocol_http1
 
   default_action {
     type             = "forward"
@@ -53,5 +48,5 @@ resource "aws_lb_listener" "minajeong_lblist" {
 resource "aws_lb_target_group_attachment" "minajeong_lbtg_att" {
   target_group_arn = aws_lb_target_group.minajeong_lbtg.arn
   target_id        = aws_instance.minajeong_weba.id
-  port             = var.http_port
+  port             = var.port_http
 }
