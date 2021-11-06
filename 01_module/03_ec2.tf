5039d27b51d387e91f798a835362cae3e0403a5a
majeong <alsdk5957@gmail.com>
2021-11-06 15:39:59 +0900

    step3.ec2

--- 01_module/03_ec2.tf
+++ 01_module/03_ec2.tf
@@ -1,15 +1,15 @@
 # Security Group
 resource "aws_security_group" "minajeong_websg" {
   name        = "Allow-WEB"
-  description = "http-ssh-icmp"
+  description = "http-ssh-icmp-mysql"
   vpc_id      = aws_vpc.minajeong_vpc.id
 
   ingress = [
     {
-      description      = var.prot_ssh
-      from_port        = var.ssh_port
-      to_port          = var.ssh_port
-      protocol         = var.prot_tcp
+      description      = var.protocol_ssh
+      from_port        = var.port_ssh
+      to_port          = var.port_ssh
+      protocol         = var.protocol_tcp
       cidr_blocks      = [var.cidr]
       ipv6_cidr_blocks = [var.cidr_v6]
       prefix_list_ids  = null
@@ -17,10 +17,10 @@ resource "aws_security_group" "minajeong_websg" {
       self             = null
     },
     {
-      description      = var.prot_http
-      from_port        = var.http_port
-      to_port          = var.http_port
-      protocol         = var.prot_tcp
+      description      = var.protocol_http
+      from_port        = var.port_http
+      to_port          = var.port_http
+      protocol         = var.protocol_tcp
       cidr_blocks      = [var.cidr]
       ipv6_cidr_blocks = [var.cidr_v6]
       prefix_list_ids  = null
@@ -28,10 +28,10 @@ resource "aws_security_group" "minajeong_websg" {
       self             = null
     },
     {
-      description      = var.prot_icmp
-      from_port        = var.icmp_port
-      to_port          = var.icmp_port
-      protocol         = var.prot_icmp
+      description      = var.protocol_icmp
+      from_port        = var.port_minus
+      to_port          = var.port_minus
+      protocol         = var.protocol_icmp
       cidr_blocks      = [var.cidr]
       ipv6_cidr_blocks = [var.cidr_v6]
       prefix_list_ids  = null
@@ -39,10 +39,10 @@ resource "aws_security_group" "minajeong_websg" {
       self             = null
     },
     {
-      description      = var.prot_mysql
-      from_port        = var.mysql_port
-      to_port          = var.mysql_port
-      protocol         = var.prot_tcp
+      description      = var.db_name
+      from_port        = var.port_mysql
+      to_port          = var.port_mysql
+      protocol         = var.protocol_tcp
       cidr_blocks      = [var.cidr]
       ipv6_cidr_blocks = [var.cidr_v6]
       security_groups  = null
@@ -53,10 +53,10 @@ resource "aws_security_group" "minajeong_websg" {
 
   egress = [
     {
-      description      = var.prot_all
+      description      = "All"
       from_port        = 0
       to_port          = 0
-      protocol         = "-1"
+      protocol         = var.protocol_minus
       cidr_blocks      = [var.cidr]
       ipv6_cidr_blocks = [var.cidr_v6]
       prefix_list_ids  = null
@@ -93,9 +93,9 @@ resource "aws_instance" "minajeong_weba" {
   key_name               = var.key
   vpc_security_group_ids = [aws_security_group.minajeong_websg.id]
   availability_zone      = "${var.region}${var.avazone[0]}"
-  private_ip             = var.private_ip
+  # private_ip             = var.private_ip
   subnet_id              = aws_subnet.minajeong_pub[0].id
-  user_data              = file("./install_seoul.sh")
+  user_data              = file("../01_module/install_seoul.sh")
 
   tags = {
     Name = "${var.name}-weba"
@@ -104,8 +104,8 @@ resource "aws_instance" "minajeong_weba" {
 
 # Elastic IP
 resource "aws_eip" "minajeong_web_eip" {
-  vpc                       = true
-  instance                  = aws_instance.minajeong_weba.id
-  associate_with_private_ip = var.private_ip
-  depends_on                = [aws_internet_gateway.minajeong_ig]
+  vpc          = true
+  instance     = aws_instance.minajeong_weba.id
+  # associate_with_private_ip = var.private_ip
+  depends_on   = [aws_internet_gateway.minajeong_ig]
 }
