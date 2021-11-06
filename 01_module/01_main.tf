511837106f3d3845189f66044a1833124f613e37
majeong <alsdk5957@gmail.com>
2021-11-06 14:58:12 +0900

    step1_main

--- 01_module/01_main.tf
+++ 01_module/01_main.tf
@@ -5,16 +5,16 @@ provider "aws" {
 
 # ssh_key
 resource "aws_key_pair" "minajeong_key" {
-  key_name = var.key
+  key_name   = var.key
   public_key = file("../../../.ssh/id_rsa.pub")
 }
 
 # VPC
 resource "aws_vpc" "minajeong_vpc" {
-  cidr_block = var.cidr_main
-  enable_dns_support = true
+  cidr_block           = var.cidr_main
+  enable_dns_support   = true
   enable_dns_hostnames = true
-  
+
   tags = {
     "Name" = "${var.name}-vpc"
   }
@@ -23,8 +23,8 @@ resource "aws_vpc" "minajeong_vpc" {
 # Public subnet
 resource "aws_subnet" "minajeong_pub" {
   vpc_id            = aws_vpc.minajeong_vpc.id
-  count             = "${length(var.public_s)}" #2
-  cidr_block        = "${var.public_s[count.index]}"
+  count             = length(var.public_s) #2
+  cidr_block        = var.public_s[count.index]
   availability_zone = "${var.region}${var.avazone[count.index]}"
 
   tags = {
@@ -35,8 +35,8 @@ resource "aws_subnet" "minajeong_pub" {
 # Private subnet
 resource "aws_subnet" "minajeong_pri" {
   vpc_id            = aws_vpc.minajeong_vpc.id
-  count             = "${length(var.private_s)}" #2
-  cidr_block        = "${var.private_s[count.index]}"
+  count             = length(var.private_s) #2
+  cidr_block        = var.private_s[count.index]
   availability_zone = "${var.region}${var.avazone[count.index]}"
 
   tags = {
@@ -47,8 +47,8 @@ resource "aws_subnet" "minajeong_pri" {
 # Private DB subnet
 resource "aws_subnet" "minajeong_pridb" {
   vpc_id            = aws_vpc.minajeong_vpc.id
-  count             = "${length(var.private_dbs)}" #2
-  cidr_block        = "${var.private_dbs[count.index]}"
+  count             = length(var.private_dbs) #2
+  cidr_block        = var.private_dbs[count.index]
   availability_zone = "${var.region}${var.avazone[count.index]}"
 
   tags = {
@@ -70,20 +70,20 @@ resource "aws_route_table" "minajeong_rt" {
   vpc_id = aws_vpc.minajeong_vpc.id
 
   route {
-  #  carrier_gateway_id = "value"
+    #  carrier_gateway_id = "value"
     cidr_block = var.cidr
-  #  destination_prefix_list_id = "value"
-  #  egress_only_gateway_id = "value"
+    #  destination_prefix_list_id = "value"
+    #  egress_only_gateway_id = "value"
     gateway_id = aws_internet_gateway.minajeong_ig.id
-  #  instance_id = "value"
-  #  ipv6_cidr_block = "value"
-  #  local_gateway_id = "value"
-  #  nat_gateway_id = "value"
-  #  network_interface_id = "value"
-  #  transit_gateway_id = "value"
-  #  vpc_endpoint_id = "value"
-  #  vpc_peering_connection_id = "value"
-  } 
+    #  instance_id = "value"
+    #  ipv6_cidr_block = "value"
+    #  local_gateway_id = "value"
+    #  nat_gateway_id = "value"
+    #  network_interface_id = "value"
+    #  transit_gateway_id = "value"
+    #  vpc_endpoint_id = "value"
+    #  vpc_peering_connection_id = "value"
+  }
   tags = {
     Name = "${var.name}-rt"
   }
@@ -91,7 +91,18 @@ resource "aws_route_table" "minajeong_rt" {
 
 # Route table association
 resource "aws_route_table_association" "minajeong_rtas_a" {
-  count = "${length(var.public_s)}"
-  subnet_id = aws_subnet.minajeong_pub[count.index].id
+  count          = length(var.public_s)
+  subnet_id      = aws_subnet.minajeong_pub[count.index].id
   route_table_id = aws_route_table.minajeong_rt.id
+}
+
+# 출력
+output {
+  description = "The public IP address of the web server"
+  value       = aws_instance.minajeong_weba.public_ip
+}
+
+output {
+  description = "The domain name of the load balancer"
+  value       = aws_lb.minajeong_lb.dns_name
 }
