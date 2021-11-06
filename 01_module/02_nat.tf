82131c67b0e897e82d042c3e4e8b9c6e89e3ad15
majeong <alsdk5957@gmail.com>
2021-11-06 15:06:50 +0900

    step2.natgatway

--- 01_module/02_nat.tf
+++ 01_module/02_nat.tf
@@ -1,8 +1,7 @@
 # Nat gateway
 resource "aws_eip" "lb_ip" {
   #instance = aws_instance.web.id
-  count = 2
-  vpc = true
+  vpc   = true
 }
 
 resource "aws_nat_gateway" "minajeong_nga" {
@@ -11,27 +10,26 @@ resource "aws_nat_gateway" "minajeong_nga" {
 
   tags = {
     "Name" = "${var.name}-ng"
-   }
+  }
+}
+# Nat gateway_route table
+resource "aws_route_table" "minajeong_ngart" {
+  vpc_id = aws_vpc.minajeong_vpc.id
+
+  route {
+    cidr_block = var.cidr
+    gateway_id = aws_nat_gateway.minajeong_nga.id
   }
 
-  # Nat gateway_route table
-  resource "aws_route_table" "minajeong_ngart" {
-    vpc_id = aws_vpc.minajeong_vpc.id
-    
-    route {
-        cidr_block  = var.cidr
-        gateway_id  = aws_nat_gateway.minajeong_nga.id
-    }
-
-    tags = {
-      "Name" = "${var.name}-nga-rt"
-    }
+  tags = {
+    "Name" = "${var.name}-ng-rt"
+  }
 }
 
 
 # Nat gateway_route table association
 resource "aws_route_table_association" "minajeong_ngartas" {
-    count            = "${length(var.private_s)}"
-    subnet_id        = aws_subnet.minajeong_pri[count.index].id
-    route_table_id   = aws_route_table.minajeong_ngart.id  
+  count          = "${length(var.private_s)}"
+  subnet_id      = aws_subnet.minajeong_pri[count.index].id
+  route_table_id = aws_route_table.minajeong_ngart.id
 }
